6f26d4a3fac28480bb6fcbb2697cd8acc1f0c300
Alexander Laughlin <44688156+AlexanderLaughlin@users.noreply.github.com>
2019-09-10 07:55:08 -0400

    Update myList.sh

--- myList.sh
+++ myList.sh
@@ -5,12 +5,12 @@ currentTime=$(date +"%x %r %Z")
 
 currentDirectory() 
 {
-    ls
+    ls           #list files in directory
 }
 
 testAutoDirectory()
 {
-	cd TestAutomation
+	cd TestAutomation     #change directory
 	ls
 }
 
@@ -30,8 +30,8 @@ cat <<- _FILE_ > file.html
 		$(testAutoDirectory)
 		</br>
 		</br>
-	<h2>Thank you for executing my script!</h2>
-	<img src="./TestAutomation/img/pair-programming.gif">
+	<h2>Thank you for executing my script!</h2>      #header
+	<img src="./TestAutomation/img/pair-programming.gif">    #paired programming gif
     </body>
     </html>
 _FILE_
